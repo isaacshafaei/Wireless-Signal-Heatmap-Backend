f39adad7ef17d4e4ec33609660ffafbc9851d11f
Isaac <isaacspirit333@gmail.com>
2025-07-13 23:58:19 +0200

    add frontend note in readme.md

--- migrations/Migration20250622221540.ts
+++ migrations/Migration20250622221540.ts
@@ -10,7 +10,20 @@ export class Migration20250622221540 extends Migration {
     this.addSql(`create table "wifi" ("id" serial primary key, "created_at" date not null, "updated_at" date not null, "user_id" int not null, "name" varchar(255) not null, "bssid" varchar(255) not null, "distance" numeric(10,0) not null, "level" numeric(10,0) not null, "security" varchar(255) not null, "frequency" numeric(10,0) not null, "lat" numeric(10,0) not null, "lng" numeric(10,0) not null, "accuracy" numeric(10,0) not null, "city" varchar(255) not null, "zipcode" varchar(255) not null, "street_name" varchar(255) not null, "street_number" varchar(255) not null, "country_code" varchar(255) not null);`);
 
     this.addSql(`alter table "wifi" add constraint "wifi_user_id_foreign" foreign key ("user_id") references "users" ("id") on update cascade;`);
+  
+    this.addSql(`
+    create table "heatmaps" (
+      "id" serial primary key,
+      "user_id" int not null,
+      "ssid" varchar(255),
+      "grid_info" jsonb,
+      "signal_grid" jsonb,
+      "floor_plan_image" bytea,
+      "created_at" timestamp default now()
+    );
+  `);
   }
+
 
   override async down(): Promise<void> {
     this.addSql(`alter table "wifi" drop constraint "wifi_user_id_foreign";`);
@@ -20,6 +33,11 @@ export class Migration20250622221540 extends Migration {
     this.addSql(`drop table if exists "users" cascade;`);
 
     this.addSql(`drop table if exists "wifi" cascade;`);
+    
+    this.addSql(`alter table "heatmaps" drop constraint "heatmaps_user_id_foreign";`);
+    this.addSql(`drop table if exists "heatmaps" cascade;`);
+
+  
   }
 
 }
